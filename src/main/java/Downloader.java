6c4444adc037d9b92fab1c72d9d4599caac5ee7d
Nicolas <sirac.nicolas@orange.fr>
2017-05-30 17:48:11 +0200

    Add runnable and size of file (last sout)

--- src/main/java/Downloader.java
+++ src/main/java/Downloader.java
@@ -9,23 +9,39 @@ import java.nio.channels.ReadableByteChannel;
 /**
  * Created by kokoghlanian on 27/04/2017.
  */
-public class Downloader {
-    public static void getFile(String host,String fileName)
-    {
-        URLConnection website = null;
-       try {
-            File file = new File(fileName);
-            System.out.println(file.length()+" 0");
-           website = new URL(host).openConnection();
-            if(file.exists())
-                website.setRequestProperty("Range", "bytes=" + file.length() + "-");
-            FileChannel download = new FileOutputStream(file,file.exists()).getChannel();
+public class Downloader implements Runnable{
+    String directory;
+    String fileName;
+    String host;
 
+    public Downloader(String directory, String fileName, String host) {
+        this.directory = directory;
+        this.fileName = fileName;
+        this.host = host;
+    }
+
+    public void getFile()
+    {File file = new File(directory+"/"+fileName);
+        System.out.println("in");
+        URLConnection website = null;
+        String filePath= directory.equals("")?fileName:directory+"/"+fileName;
+        System.out.println("out");
+       try {
+
+            //System.out.println(file.length()+" 0");
+           website = new URL(host).openConnection();
+            if(file.exists()) {
+                System.out.println("in");
+                website.setRequestProperty("Range", "bytes=" + file.length() + "-");
+            }
+           System.out.println("in");
+            FileChannel download = new FileOutputStream(file,file.exists()).getChannel();
 
             ReadableByteChannel rbc = Channels.newChannel(website.getInputStream());
 
+           System.out.println(website.getContentLengthLong());
 
-           download.transferFrom(rbc, file.length(), Long.MAX_VALUE);
+          // download.transferFrom(rbc, file.length(), Long.MAX_VALUE);
     } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
@@ -35,9 +51,15 @@ public class Downloader {
         }
 
     }
-
+    public void run() {
+        getFile();
+    }
     public static void main(String[] args)
     {
-        getFile("http://www7.uptobox.com/d/vay62japhsr76xkqugcmobreou4nigqeinigtpzdsl6ajlheroriiskycj3zpwjzrge3kont4tcqrjqeve/Lion.2016.FRENCH.BRRip.XviD-NEWCiNE-WwW.Zone-Telechargement.Ws.avi","screen.jpg");
+        Downloader downloader= new Downloader("C:/Users/Nicolas/Desktop","video.avi","http://www18.uptobox.com/d/wmytzdl4hwr76xkqrkp7e3ryes32pnwnicpvt2ihn7y76tt5kowitwbynrti4qoqlfa6njpvwaymddy/Logan.2017.FRENCH.BRRip.XviD.AC3-NEWCiNE-WwW.Zone-Telechargement.Ws.avi");
+        Thread t = new Thread(downloader);
+        t.start();
     }
+
+
 }
