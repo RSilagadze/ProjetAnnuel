6b4b1865450e447205ef1cc14b1d160c715af16e
Nicolas <nicolas.sirac@orange.fr>
2017-06-07 10:46:51 +0200

    Multithread console download

--- src/main/java/Downloader.java
+++ src/main/java/Downloader.java
@@ -5,6 +5,7 @@ import java.net.URLConnection;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.util.Scanner;
 
 /**
  * Created by kokoghlanian on 27/04/2017.
@@ -21,27 +22,23 @@ public class Downloader implements Runnable{
     }
 
     public void getFile()
-    {File file = new File(directory+"/"+fileName);
-        System.out.println("in");
+    {
+        File file = new File(directory+"/"+fileName);
         URLConnection website = null;
         String filePath= directory.equals("")?fileName:directory+"/"+fileName;
-        System.out.println("out");
        try {
 
-            //System.out.println(file.length()+" 0");
            website = new URL(host).openConnection();
+           if(file.length() != website.getContentLengthLong()){
             if(file.exists()) {
-                System.out.println("in");
                 website.setRequestProperty("Range", "bytes=" + file.length() + "-");
             }
-           System.out.println("in");
             FileChannel download = new FileOutputStream(file,file.exists()).getChannel();
 
             ReadableByteChannel rbc = Channels.newChannel(website.getInputStream());
-
-           System.out.println(website.getContentLengthLong());
-
-          // download.transferFrom(rbc, file.length(), Long.MAX_VALUE);
+            download.transferFrom(rbc, file.length(), Long.MAX_VALUE);
+            download.close();
+           }
     } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (FileNotFoundException e) {
@@ -56,9 +53,23 @@ public class Downloader implements Runnable{
     }
     public static void main(String[] args)
     {
-        Downloader downloader= new Downloader("C:/Users/Nicolas/Desktop","video.avi","http://www18.uptobox.com/d/wmytzdl4hwr76xkqrkp7e3ryes32pnwnicpvt2ihn7y76tt5kowitwbynrti4qoqlfa6njpvwaymddy/Logan.2017.FRENCH.BRRip.XviD.AC3-NEWCiNE-WwW.Zone-Telechargement.Ws.avi");
-        Thread t = new Thread(downloader);
-        t.start();
+        System.out.println("Entrez stop pour arreter le programme (cela ne stopera pas le téléchargement de vos fichier)");
+        Scanner sc = new Scanner(System.in);
+        String enter = "";
+        while (!enter.equalsIgnoreCase("stop")) {
+            System.out.println("Enter the directory, the file name and the download link separated by a space");
+            enter=sc.nextLine();
+            if(!enter.equalsIgnoreCase("stop")) {
+                Downloader downloader = new Downloader(enter.split(" ")[0], enter.split(" ")[1], enter.split(" ")[2]);
+                Thread t = new Thread(downloader);
+                t.start();
+                try {
+                    t.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
